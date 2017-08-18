2275a3d157693abe6f31f311e2667d42b8d95914
Paola Ortiz <paogi_ortiz@hotmail.com>
2017-08-18 10:18:29 -0500

    Pruebas unitarias mocha

--- tests.js
+++ tests.js
@@ -1,9 +1,16 @@
-describe('DOM tests - Boton Creditos ', function() {
-  var formElem = document.forms[0];
-  var creditosButton = document.getElementById('creditos');
- 
+describe('DOM tests - Boton Creditos ', function () {
+    var formElem = document.forms[0];
+    var creditosButton = document.getElementById('creditos');
+    var titulo = document.getElementById('titulo')
+
+
+    it('El boton creditos tiene el texto correcto', function () {
+        expect(creditosButton.innerHTML).to.equal('Creditos');
+    });
+    
+    it('La etiqueta titulo tiene el nombre correcto', function () {
+        expect(titulo.innerHTML).to.equal('Miauhistory');
+    });
   
-  it('El boton creditos tiene el texto correcto', function() {
-    expect(creditosButton.innerHTML).to.equal('Creditos');
-  });
+ 
 });
